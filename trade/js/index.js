8b0dd9aba557bac57b7bbfae5914c54ba3737cc7
Cubity_First <cubity@cubityfir.st>
2019-01-01 22:54:45 +0000

    Stuff broke, then i fixed it, uploading for Fer

--- trade/js/index.js
+++ trade/js/index.js
@@ -1,5 +1,4 @@
-//I'm so sorry any of this has to be done the way it was. 
-
+//I'm so sorry any of this has to be done the way it was.
 $.fn.extend({
     animateCss: function (animationName, callback) {
         var animationEnd = (function (el) {
@@ -28,34 +27,32 @@ $.fn.extend({
 });
 
 function hide() {
-    var div = document.getElementsByClassName("main")[0];
-    var infoDiv = $(div.getElementsByClassName("infoDiv")[0]);
+    var div = $('.main');
+    var infoDiv = $('.infoDiv');
     infoDiv.text('󠀡');
     document.getElementById("hideMe").style.color = '#242424';
 }
 
 function showDiscord() {
-    var div = document.getElementsByClassName("main")[0];
-    var infoDiv = $(div.getElementsByClassName("infoDiv")[0]);
+    var div = $('.main');
+    var infoDiv = $('.infoDiv');
     infoDiv.animateCss('tada');
     infoDiv.text('Cubity_First#0001');
     document.getElementById("hideMe").style.color = 'white';
 }
 
 function showBackpack() {
-    var div = document.getElementsByClassName("main")[0];
-    var infoDiv = $(div.getElementsByClassName("infoDiv")[0]);
+    var div = $('.main');
+    var infoDiv = $('.infoDiv');
     infoDiv.animateCss('tada');
     infoDiv.text('cubityfir.st/bp');
     document.getElementById("hideMe").style.color = 'white';
 }
 
 function showSteam() {
-    var div = document.getElementsByClassName("main")[0];
-    var infoDiv = $(div.getElementsByClassName("infoDiv")[0]);
+    var div = $('.main');
+    var infoDiv = $('.infoDiv');
     infoDiv.animateCss('tada');
     infoDiv.text('cubityfir.st/offer');
     document.getElementById("hideMe").style.color = 'white';
 }
-
-
