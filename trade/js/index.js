f57c7ab034481c111d3ad6054523da3963ee9231
Cubity_First <cubity@cubityfir.st>
2019-01-03 12:45:43 +0000

    Minor Fix

--- trade/js/index.js
+++ trade/js/index.js
@@ -24,7 +24,7 @@ $.fn.extend({
 $(document).ready(function () {
     var infoDiv = $('.infoDiv');
 
-    changeColor = function () {
+    changeColor = function (color) {
         document.getElementById("hideMe").style.color = 'white';
     }
 
