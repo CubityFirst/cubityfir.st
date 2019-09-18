75c5393fd292c862b2339b937cc9ae7f48c1f805
Cubity_First <cubity@cubityfir.st>
2019-09-18 15:57:11 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+function switchVisible() {
+            if (document.getElementById('box')) {
+	                if (document.getElementById('box').style.display == 'none') {
+                    document.getElementById('box').style.display = 'block';
+                    document.getElementById('dirbox').style.display = 'none';
+                }
+                else {
+                    document.getElementById('box').style.display = 'none';
+                    document.getElementById('dirbox').style.display = 'block';
+                }
+            }
+}
+
 /**
  * requestAnimationFrame
  */
