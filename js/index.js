7c5b879497765f1649ca58fffef0d221e46ec5f3
Cubity_First <cubity@cubityfir.st>
2019-09-23 16:22:18 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,21 +11,10 @@ function switchVisible() {
             }
 }
 
-// a key map of allowed keys
-var allowedKeys = {
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down',
-  65: 'a',
-  66: 'b'
-};
-
-// the 'official' Konami Code sequence
-var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
-
-// a variable to remember the 'position' the user has reached so far.
-var konamiCodePosition = 0;
+var allowedKeys = {37: 'l',38: 'u',39: 'r',40: 'd',65: 'a',66: 'b',
+		  45: 'n',45: 'n',45: 'n',45: 'n',45: 'n',45: 'n',};
+var konamiCode = ['u', 'u', 'd', 'd', 'l', 'r', 'l', 'r', 'b', 'a'];
+var konamiCodeStatus = 0;
 
 // add keydown event listener
 document.addEventListener('keydown', function(e) {
@@ -36,13 +25,13 @@ document.addEventListener('keydown', function(e) {
 
   // compare the key with the required key
   if (key == requiredKey) {
-    konamiCodePosition++;
-    if (konamiCodePosition == konamiCode.length) {
+    konamiCodeStatus++;
+    if (konamiCodeStatus == konamiCode.length) {
       activateCheats();
-      konamiCodePosition = 0;
+      konamiCodeStatus = 0;
     }
   } else {
-    konamiCodePosition = 0;
+    konamiCodeStatus = 0;
   }
 });
 
